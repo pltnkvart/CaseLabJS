e9bea5a277f77a6660705431c191a4c9530a5e05
pltnkvart <artosi@yandex.ru>
2023-08-22 18:45:06 +0300

    added form and css styles

---
+++ index.js
@@ -0,0 +1,33 @@
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function updateList() {
+
+}
+
+function addTask() {
+
+}
+
+function completeTask(index) {
+
+}
+
+function deleteTask(index) {
+
+}
+
+function highlightEven() {
+
+}
+
+function highlightOdd() {
+
+}
+
+function deleteFirst() {
+
+}
+
+function deleteLast() {
+
+}
